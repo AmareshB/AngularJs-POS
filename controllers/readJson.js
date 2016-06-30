7ea961f630f6e952a3151fdbaa76021ce28e0f1f
Amaresh <amareshbingumalla@gmail.com>
2016-07-01 01:33:26 +0530

    Added gencontrols function

--- controllers/readJson.js
+++ controllers/readJson.js
@@ -2,32 +2,63 @@ var readData = angular.module('myApp.readJson',[]);
 
 readData.controller('readJsonController', ['$scope','$http','$sce' ,function($scope,$http,$sce){
 	$scope.showData = function(){
-		var promise = $http.get('https://api.myjson.com/bins/4fcqf');
-		promise.success(function(data){
-			//console.log(data);
+		var result = $http.get('https://api.myjson.com/bins/2ctn1');
+		result.success(function(data){
 			console.log(data.prompt);
 			var htmlContent = '';
 			for (var i = 0; i < data.prompt.length; i++) {
-				$scope.control = data.prompt[i];
-				if ($scope.control.dispType == 'textfield') {
-						$scope.showtextfield = true;
-						htmlContent += genText(data.prompt[i]);
-						$scope.htmlContent = $sce.trustAsHtml(htmlContent);
-						console.log($scope.htmlContent);
-				}
+				htmlContent += genControls(data.prompt[i] , data);
 			}
+			$scope.htmlContent = $sce.trustAsHtml(htmlContent);
 		});
-		promise.error(function(data,status,error,config){
-			console.log("Its an error" , data);
+		result.error(function(data,status,error,config){
+			console.log("It's an error" , data);
 		});
 	};
-	var genText = function(prompt) {
+
+	var genControls = function(prompt,responseData) {
+		var control , htmlContent = '';
+				control = prompt;
+				if (control.dispType == 'textfield') {
+					htmlContent = genText(prompt , responseData);
+				} else if(control.dispType === 'radio') {
+					htmlContent = genRadio(prompt , responseData);
+				} else if (control.dispType === 'button') {
+					htmlContent = genButton(prompt , responseData);
+				}
+				//$scope.htmlContent = $sce.trustAsHtml(htmlContent);
+			
+		return htmlContent;	
+	}
+	var genText = function(prompt,responseData) {
 		var htmlContent = '<div>' + prompt.label+' : <input type="text" name='+prompt.label+'>' + '</div>';
+		if(prompt.target!== ''){
+			var target = prompt.target;
+			showTarget(target,responseData);
+		}
 		console.log('htmlContent : ' + htmlContent);
-		return $sce.trustAsHtml(htmlContent);
+		return htmlContent;
+	}
+
+	var genRadio = function(prompt,responseData) {
+		var htmlContent = '<h3 id ='+prompt.id+' >'+ prompt.content+'</h3>';
+		var answer = prompt.answers.answer;
+		for(var i =0; i< answer.length;i++) {
+			console.log('No of answers');
+			htmlContent += '<input type = "radio" id = "'+answer[i].id+'"name = "'+prompt.answers.id+
+			'"value="'+answer[i].value+'"">'+answer[i].content+'</input><br/>';
+		}
+  		return htmlContent;
+
 
 	}
-	
+	 var showTarget = function(target,responseData) {
+	 	for (var i = 0; i < responseData.prompt.length; i++) {
+	 		if(responseData.prompt.id === 'target' ) {
+
+	 		}
+	 	}
+	 }
 
 	$scope.showControls = function(){
 		console.log(jsondata);
